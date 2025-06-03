9940d3d0359892e588992630d7ab7eca597ff32d
liuc <348226387@qq.com>
2025-06-03 08:56:01 +0800

    feat(Demo): 添加人员信息、部门信息和培训信息
    - 在 Demo 文件中增加了人员信息、部门信息和培训信息的示例数据
    - 使用 Map 对象存储各类信息，并将其传递给模板进行渲染
    - 此修改丰富了示例内容，展示了更多模板变量的使用方式

--- src/main/java/com/skmj/server/poitl/Demo.java
+++ src/main/java/com/skmj/server/poitl/Demo.java
@@ -6,6 +6,7 @@ import com.deepoove.poi.util.PoitlIOUtils;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author lc
@@ -18,6 +19,33 @@ public class Demo {
                     new HashMap<String, Object>(1){{
                         put("title", "Hi, poi-tl Word模板引擎");
                     }});
+            Map<String, Object> map = new HashMap<String, Object>();
+            //人员信息
+            map.put("name", "张三");
+            map.put("sex", "男");
+            map.put("age", "18");
+            map.put("address", "北京");
+            map.put("phone", "12345678901");
+            map.put("email", "12345678901@163.com");
+            map.put("date", "2020-01-01");
+            map.put("remark", "备注");
+            //部门信息
+            map.put("department", "技术部");
+            map.put("position", "Java开发");
+            map.put("work_date", "2020-01-01");
+            map.put("salary", "5000");
+            map.put("bonus", "500");
+            map.put("tax", "500");
+            map.put("total", "5500");
+            //培训信息
+            map.put("training", "培训");
+            map.put("training_name", "培训名称");
+            map.put("training_date", "2020-01-01");
+            map.put("training_content", "培训内容");
+            map.put("training_result", "培训结果");
+            map.put("training_remark", "培训备注");
+            template.render(map);
+
             FileOutputStream fileOutputStream = new FileOutputStream("D:\\output.docx");
             template.write(fileOutputStream);
             PoitlIOUtils.closeQuietlyMulti(template, fileOutputStream);
